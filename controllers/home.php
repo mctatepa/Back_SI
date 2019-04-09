610414955c75707371109f75a19cccc439dfc960
Baptistebarel <32815239+Baptistebarel@users.noreply.github.com>
2019-04-09 15:32:05 +0200

    :rocket: Deploying IMDB cache :rocket:

--- controllers/home.php
+++ controllers/home.php
@@ -1,8 +1,8 @@
 <?php
 
-$title = 'My Website';
+$title = "Move'in";
 
-// Define city and use Paris as default
+// Get movies and use fight club as default
 $movie = empty($_POST['movie']) ? 'fight club' : $_POST['movie'];
 
 // Create API url
@@ -14,23 +14,11 @@ $movieUrl .= http_build_query([
   'page' => 1,
 ]);
 
-
-//
-//  REQUEST IMDB (RAPIDAPI)
-//
-
-require_once '../unirest-php-master/src/Unirest.php';
-
-$movieUrl2 = Unirest\Request::get("https://movie-database-imdb-alternative.p.rapidapi.com/?s='./$movie/'",
-  array(
-    "X-RapidAPI-Host" => "movie-database-imdb-alternative.p.rapidapi.com",
-    "X-RapidAPI-Key" => "121e39e918mshbaf8ba77d175913p1ec8c6jsnd0dc695e72dd"
-  )
-);
+// CACHE TMDB
 
 // Create cache info
 $cacheKey = $movie;
-$cachePath = '../cache/'.$cacheKey;
+$cachePath = '../cache/cache_TMDB/'.$cacheKey;
 $cacheUsed = false;
 
 
@@ -51,8 +39,48 @@ else{
 }
 // Show result
 
-// Decode JSON
-$result = json_decode($result);
+    // Decode JSON
+    $result = json_decode($result);
+
+
+//
+//  REQUEST IMDB (RAPIDAPI)
+//
+
+require_once '../unirest-php-master/src/Unirest.php';
+
+// Creation of the IMDB ID array
+
+$imdbId = [];
+
+// Create cache info
+$cacheKey2 = $movie;
+$cachePath2 = '../cache/cache_IMDB/'.$cacheKey2;
+$cacheUsed2 = false;
+
+
+if(file_exists($cachePath2) && time() - filemtime($cachePath2) < (60*60*24))
+{
+  $result2 = file_get_contents($cachePath2);
+  $cacheUsed2 = true;
+}
+else{
+
+  $result2 = Unirest\Request::get("https://movie-database-imdb-alternative.p.rapidapi.com/?s='./$movie/'",
+  array(
+    "X-RapidAPI-Host" => "movie-database-imdb-alternative.p.rapidapi.com",
+    "X-RapidAPI-Key" => "121e39e918mshbaf8ba77d175913p1ec8c6jsnd0dc695e72dd"
+  )
+);
+
+  $result2 = json_encode($result2);
+
+  // Save in cache
+  file_put_contents($cachePath2, $result2);
+}
+$result2 = json_decode($result2);
+
+
 
 $genres = array(
   28  => "Action",
@@ -77,7 +105,7 @@ $genres = array(
 );
 
 echo '<pre>';
-print_r($result);
+print_r($result2);
 echo '</pre>';
 
   echo '<pre>';
